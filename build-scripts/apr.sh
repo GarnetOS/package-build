ccea6daeb22fe53b469e41f6595789c5b3cd72d6
radimkohout <radimm.kohout@gmail.com>
2020-10-31 11:50:17 +0100

    Update apr.sh

--- build-scripts/apr.sh
+++ build-scripts/apr.sh
@@ -3,7 +3,7 @@ sleep 10
 #ping 8.8.8.8
 dhclient -r
 dhclient
-wget ftp://ftp.mirrorservice.org/sites/ftp.apache.org/apr/apr-1.7.0.tar.bz2
+wget https://archive.apache.org/dist/apr/apr-1.7.0.tar.bz2
 tar -xvf apr-1.7.0.tar.bz2	
 cd apr-1.7.0	
 ./configure --prefix=/usr --disable-static --with-installbuilddir=/usr/share/apr-1/build	
