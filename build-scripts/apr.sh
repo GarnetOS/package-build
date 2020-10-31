804546c5a46489d52bdf856d40aac04551de1f14
radimkohout <radimm.kohout@gmail.com>
2020-10-31 11:30:56 +0100

    Update apr.sh

--- build-scripts/apr.sh
+++ build-scripts/apr.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 sleep 10
-ping 8.8.8.8
+#ping 8.8.8.8
+rdate -uns 83.91.10.45
 wget ftp://ftp.mirrorservice.org/sites/ftp.apache.org/apr/apr-1.7.0.tar.bz2
 tar -xvf apr-1.7.0.tar.bz2	
 cd apr-1.7.0	
