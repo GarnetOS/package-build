4b166d4c55ad10176e470b16b0b9e9e0faae2693
radimkohout <radimm.kohout@gmail.com>
2020-10-31 18:26:37 +0100

    Update apr.sh

--- build-scripts/apr.sh
+++ build-scripts/apr.sh
@@ -1,8 +1,4 @@
 #/bin/sh
-sleep 10
-#ping 8.8.8.8
-dhclient -r
-dhclient
 wget https://archive.apache.org/dist/apr/apr-1.7.0.tar.bz2
 tar -xvf apr-1.7.0.tar.bz2	
 cd apr-1.7.0	
@@ -11,3 +7,5 @@ make
 make install
 cd ..
 rm -rf apr-1.7.0
+rm apr-1.7.0.tar.bz2
+build-scripts/empty.sh
