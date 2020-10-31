6872ab13d065fc68f94507038c26bafc6ca813a8
radimkohout <radimm.kohout@gmail.com>
2020-10-31 10:24:56 +0100

    Update apr.sh

--- build-scripts/apr.sh
+++ build-scripts/apr.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-wget https://archive.apache.org/dist/apr/apr-1.7.0.tar.bz2	
+wget ftp://ftp.mirrorservice.org/sites/ftp.apache.org/apr/apr-1.7.0.tar.bz2
 tar -xvf apr-1.7.0.tar.bz2	
 cd apr-1.7.0	
 ./configure --prefix=/usr    \	
