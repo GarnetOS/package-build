c8ba9a5869e1e111862d7b401938aff6f545c23c
radimkohout <radimm.kohout@gmail.com>
2020-10-31 20:00:34 +0100

    Update empty.sh

--- build-scripts/empty.sh
+++ build-scripts/empty.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ls
 sudo umount -l ./chroot/dev
-for i in {1..10}; do sudo find ./chroot -type d -empty -print -delete; done
+for i in {1..10}; do echo $i; sudo find ./chroot -type d -empty -print -delete; done
