557af7334195f0c0937778a977256ac4bb6aa331
radimkohout <radimm.kohout@gmail.com>
2020-10-31 11:40:59 +0100

    Update apr.sh

--- build-scripts/apr.sh
+++ build-scripts/apr.sh
@@ -6,8 +6,6 @@ dhclient
 wget ftp://ftp.mirrorservice.org/sites/ftp.apache.org/apr/apr-1.7.0.tar.bz2
 tar -xvf apr-1.7.0.tar.bz2	
 cd apr-1.7.0	
-./configure --prefix=/usr    \	
-            --disable-static \	
-            --with-installbuilddir=/usr/share/apr-1/build &&	
+./configure --prefix=/usr --disable-static --with-installbuilddir=/usr/share/apr-1/build	
 make	
 make install
