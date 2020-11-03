59f5333003622ad64590e142c976cc26b077632e
radimkohout <radimm.kohout@gmail.com>
2020-11-03 16:43:34 +0100

    Update gccgo.sh

--- build-scripts/gccgo.sh
+++ build-scripts/gccgo.sh
@@ -12,9 +12,9 @@ cd    build
     --disable-multilib                                 \
     --with-system-zlib                                 \
     --enable-languages=go &&
-make -j4
+make -j4 -s
 ulimit -s 32768 &&
-make -k check
+make -k -s check
 ../contrib/test_summary
 make install &&
 
