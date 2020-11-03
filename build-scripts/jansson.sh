479892af5a52c8c6ebf4ddd6dcd6b0763bb32439
radimkohout <radimm.kohout@gmail.com>
2020-11-03 13:31:45 +0100

    Create jansson.sh

---
+++ build-scripts/jansson.sh
@@ -0,0 +1,10 @@
+build-scripts/prepare.sh
+wget http://www.digip.org/jansson/releases/jansson-2.13.1.tar.gz
+tar -xvf jansson-2.13.1.tar.gz
+cd jansson-2.13.1
+./configure --prefix=/usr --disable-static
+make
+make install
+cd ..
+rm jansson-2.13.1.tar.gz
+rm -rf jansson-2.13.1
