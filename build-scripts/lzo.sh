01374de4d0b49b988244b6fab800f6b096c9ab2a
radimkohout <radimm.kohout@gmail.com>
2020-11-03 09:30:13 +0100

    Create lzo.sh

---
+++ build-scripts/lzo.sh
@@ -0,0 +1,10 @@
+build-scripts/prepare.sh
+wget http://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz
+tar -xvf lzo-2.10.tar.gz
+cd lzo-2.10
+./configure --prefix=/usr --enable-shared --disable-static --docdir=/usr/share/doc/lzo-2.10
+make
+make install
+cd ..
+rm lzo-2.10.tar.gz
+rm -rf lzo-2.10
