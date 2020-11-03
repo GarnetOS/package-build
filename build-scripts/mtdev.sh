5439e21c2f05d941bbb8150de61a65806cf044fd
radimkohout <radimm.kohout@gmail.com>
2020-11-03 18:19:37 +0100

    Create mtdev.sh

---
+++ build-scripts/mtdev.sh
@@ -0,0 +1,10 @@
+build-scripts/prepare.sh
+wget http://bitmath.org/code/mtdev/mtdev-1.1.6.tar.bz2
+tar -xvf mtdev-1.1.6.tar.bz2
+cd mtdev-1.1.6
+./configure --prefix=/usr --disable-static &&
+make
+make install
+cd ..
+rm mtdev-1.1.6.tar.bz2
+rm -rf mtdev-1.1.6
