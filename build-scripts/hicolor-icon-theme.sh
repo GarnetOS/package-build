01591580dbf3db88dfbcaa3fe8dda999ff6a1b9e
radimkohout <radimm.kohout@gmail.com>
2020-11-03 18:02:45 +0100

    Create hicolor-icon-theme.sh

---
+++ build-scripts/hicolor-icon-theme.sh
@@ -0,0 +1,9 @@
+build-scripts/prepare.sh
+wget https://icon-theme.freedesktop.org/releases/hicolor-icon-theme-0.17.tar.xz
+tar -xvf hicolor-icon-theme-0.17.tar.xz
+cd hicolor-icon-theme-0.17
+./configure --prefix=/usr
+make install
+cd ..
+rm hicolor-icon-theme-0.17.tar.xz
+rm -rf hicolor-icon-theme-0.17
