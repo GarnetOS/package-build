95f41d0c84d08cd10d05488b660d3abdaf1df6db
radimkohout <radimm.kohout@gmail.com>
2020-11-03 09:22:31 +0100

    Create libaio.sh

---
+++ build-scripts/libaio.sh
@@ -0,0 +1,9 @@
+build-scripts/prepare.sh
+wget http://ftp.de.debian.org/debian/pool/main/liba/libaio/libaio_0.3.112.orig.tar.xz
+tar -xvf libaio_0.3.112.orig.tar.xz
+cd libaio_0.3.112
+sed -i '/install.*libaio.a/s/^/#/' src/Makefile
+make
+make install
+rm -rf libaio_0.3.112
+rm libaio_0.3.112.orig.tar.xz
