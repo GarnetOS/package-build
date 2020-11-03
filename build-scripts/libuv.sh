12b9c49ce1c7afff6a408eef03c6ba4663846acb
radimkohout <radimm.kohout@gmail.com>
2020-11-03 09:27:48 +0100

    Create libuv.sh

---
+++ build-scripts/libuv.sh
@@ -0,0 +1,10 @@
+build-scripts/prepare.sh
+wget https://dist.libuv.org/dist/v1.38.1/libuv-v1.38.1.tar.gz
+tar -xvf libuv-v1.38.1.tar.gz
+cd libuv-v1.38.1
+sh autogen.sh
+./configure --prefix=/usr --disable-static
+make
+make install
+rm libuv-v1.38.1.tar.gz
+rm -rf libuv-v1.38.1
