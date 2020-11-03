52786e6907acc4baf29e9fb7a17d1997ecab38f9
radimkohout <radimm.kohout@gmail.com>
2020-11-03 19:06:31 +0100

    Update gobjc++.sh

--- build-scripts/gobjc++.sh
+++ build-scripts/gobjc++.sh
@@ -14,8 +14,8 @@ cd    build
     --with-system-zlib                                 \
     --enable-languages=obj-c++ &&
 make -j4 -s
-ulimit -s 32768 &&
-make -k -s check
+#ulimit -s 32768 &&
+#make -k -s check
 ../contrib/test_summary
 make install &&
 
