a5ef61bc814612f3052c2f904c710d0177f0fe16
radimkohout <radimm.kohout@gmail.com>
2020-11-03 19:12:17 +0100

    Create libcddb.sh

---
+++ build-scripts/libcddb.sh
@@ -0,0 +1,10 @@
+build-scripts/prepare.sh
+wget https://downloads.sourceforge.net/libcddb/libcddb-1.3.2.tar.bz2
+tar -xvf libcddb-1.3.2.tar.bz2
+cd libcddb-1.3.2
+./configure --prefix=/usr --disable-static &&
+make
+make install
+cd ..
+rm libcddb-1.3.2.tar.bz2
+rm -rf libcddb-1.3.2
