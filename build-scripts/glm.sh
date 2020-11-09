597738b070d9a3151392023e693f058bcf85ffad
radimkohout <radimm.kohout@gmail.com>
2020-11-09 19:02:58 +0100

    Create glm.sh

---
+++ build-scripts/glm.sh
@@ -0,0 +1,9 @@
+build-scripts/prepare.sh
+wget https://github.com/g-truc/glm/archive/0.9.9.8/glm-0.9.9.8.tar.gz
+tar -xvf glm-0.9.9.8.tar.gz
+cd glm-0.9.9.8
+cp -r glm /usr/include/ &&
+cp -r doc /usr/share/doc/glm-0.9.9.8
+cd ..
+rm glm-0.9.9.8.tar.gz
+rm -rf glm-0.9.9.8
