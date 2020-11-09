0fec35af3e8bbf34a748649f5f72491ae6f5f7f0
radimkohout <radimm.kohout@gmail.com>
2020-11-09 18:54:11 +0100

    Create libsigsegv.sh

---
+++ build-scripts/libsigsegv.sh
@@ -0,0 +1,12 @@
+build-scripts/prepare.sh
+wget https://ftp.gnu.org/gnu/libsigsegv/libsigsegv-2.12.tar.gz
+tar -xvf libsigsegv-2.12.tar.gz
+cd libsigsegv-2.12
+./configure --prefix=/usr   \
+            --enable-shared \
+            --disable-static &&
+make
+make install
+cd ..
+rm libsigsegv-2.12.tar.gz
+rm -rf libsigsegv-2.12
