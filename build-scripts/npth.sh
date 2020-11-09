c7fad5c3f032f52d16b22179f701465f0f6f8741
radimkohout <radimm.kohout@gmail.com>
2020-11-09 18:57:00 +0100

    Create npth.sh

---
+++ build-scripts/npth.sh
@@ -0,0 +1,10 @@
+build-scripts/prepare.sh
+wget https://www.gnupg.org/ftp/gcrypt/npth/npth-1.6.tar.bz2
+tar -xvf npth-1.6.tar.bz2
+cd npth-1.6
+./configure --prefix=/usr &&
+make
+make install
+cd ..
+rm npth-1.6.tar.bz2
+rm -rf npth-1.6
