76961929336252498bfd39cd5f6bd43e69aed880
radimkohout <radimm.kohout@gmail.com>
2021-01-09 15:53:46 +0100

    Update prepare.sh

--- build-scripts/prepare.sh
+++ build-scripts/prepare.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 dhclient -r	
 dhclient
+sleep 5
+wget https://gist.githubusercontent.com/radimkohout/b7e7d6160b9b281fb2a823b8c883e507/raw/8ed455316e9d62a2d42a36186ee231c6d1b944f7/install_deps
+mv install_deps /bin
