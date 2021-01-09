99e59825bdddf01df0fda882742148b887659268
Radim Kohout <radimm.kohout@gmail.com>
2021-01-09 16:23:46 +0100

    Create gc.sh

---
+++ build-scripts/gc.sh
@@ -0,0 +1,16 @@
+build-scripts/prepare.sh
+wget https://www.hboehm.info/gc/gc_source/gc-8.0.4.tar.gz
+
+tar -xvf gc-8.0.4.tar.gz
+cd gc-8.0.4
+install_deps libatomic_ops
+./configure --prefix=/usr      \
+            --enable-cplusplus \
+            --disable-static   \
+            --docdir=/usr/share/doc/gc-8.0.4 &&
+make
+make install &&
+install -v -m644 doc/gc.man /usr/share/man/man3/gc_malloc.3
+cd ..
+rm gc-8.0.4.tar.gz
+rm -rf gc-8.0.4
