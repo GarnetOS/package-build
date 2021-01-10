6650a4eb7e4346b8ea5291755c7486732b6803ea
Radim Kohout <radimm.kohout@gmail.com>
2021-01-10 12:21:32 +0100

    Updated config and added libcdio

---
+++ build-scripts/libcdio.sh
@@ -0,0 +1,20 @@
+build-scripts/prepare.sh
+wget https://ftp.gnu.org/gnu/libcdio/libcdio-2.1.0.tar.bz2
+
+wget https://ftp.gnu.org/gnu/libcdio/libcdio-paranoia-10.2+2.0.1.tar.bz2
+
+tar -xvf libcdio-2.1.0.tar.bz2
+cd libcdio-2.1.0.
+install_deps libcddb
+./configure --prefix=/usr --disable-static &&
+make
+make install
+tar -xf ../libcdio-paranoia-10.2+2.0.1.tar.bz2 &&
+cd libcdio-paranoia-10.2+2.0.1 &&
+
+./configure --prefix=/usr --disable-static &&
+make
+make install
+cd ..
+rm libcdio-2.1.0.tar.bz2
+rm -rf libcdio-2.1.0
