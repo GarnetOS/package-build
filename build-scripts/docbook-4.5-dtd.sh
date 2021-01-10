c3fb5e62b1cd3517716f4043dee6555adc673626
Radim Kohout <radimm.kohout@gmail.com>
2021-01-10 11:41:22 +0100

    Added docbook-dtd

---
+++ build-scripts/docbook-4.5-dtd.sh
@@ -0,0 +1,34 @@
+build-scripts/prepare.sh
+install_deps sgml-common
+install_deps unzip
+wget http://www.docbook.org/sgml/4.5/docbook-4.5.zip
+
+unzip docbook-4.5.zip
+cd 
+sed -i -e '/ISO 8879/d' \
+       -e '/gml/d' docbook.cat
+install -v -d /usr/share/sgml/docbook/sgml-dtd-4.5 &&
+chown -R root:root . &&
+
+install -v docbook.cat /usr/share/sgml/docbook/sgml-dtd-4.5/catalog &&
+cp -v -af *.dtd *.mod *.dcl /usr/share/sgml/docbook/sgml-dtd-4.5 &&
+
+install-catalog --add /etc/sgml/sgml-docbook-dtd-4.5.cat \
+    /usr/share/sgml/docbook/sgml-dtd-4.5/catalog &&
+
+install-catalog --add /etc/sgml/sgml-docbook-dtd-4.5.cat \
+    /etc/sgml/sgml-docbook.cat
+cat >> /usr/share/sgml/docbook/sgml-dtd-4.5/catalog << "EOF"
+  -- Begin Single Major Version catalog changes --
+
+PUBLIC "-//OASIS//DTD DocBook V4.4//EN" "docbook.dtd"
+PUBLIC "-//OASIS//DTD DocBook V4.3//EN" "docbook.dtd"
+PUBLIC "-//OASIS//DTD DocBook V4.2//EN" "docbook.dtd"
+PUBLIC "-//OASIS//DTD DocBook V4.1//EN" "docbook.dtd"
+PUBLIC "-//OASIS//DTD DocBook V4.0//EN" "docbook.dtd"
+
+  -- End Single Major Version catalog changes --
+EOF
+cd ..
+rm docbook-4.5.zip
+rm -rf 
