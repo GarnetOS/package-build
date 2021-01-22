f826a33e8ab2a7c96643c8f85d6717392bc903f9
radimkohout <radimm.kohout@gmail.com>
2021-01-22 17:38:14 +0100

    Update libdvdnav.sh

--- build-scripts/libdvdnav.sh
+++ build-scripts/libdvdnav.sh
@@ -1,8 +1,11 @@
 build-scripts/prepare.sh
 wget https://get.videolan.org/libdvdnav/6.1.0/libdvdnav-6.1.0.tar.bz2
 tar -xvf libdvdnav-6.1.0.tar.bz2
-cd libdvdnav-6.1.0.
+cd libdvdnav-6.1.0
 install_deps libdvdread
+if [ -z "$old" ];
+then
+       exit
 ./configure --prefix=/usr    \
             --disable-static \
             --docdir=/usr/share/doc/libdvdnav-6.1.0 &&
@@ -10,4 +13,4 @@ make
 make install
 cd ..
 rm libdvdnav-6.1.0.tar.bz2
-rm -rf libdvdnav-6.1.0.
+rm -rf libdvdnav-6.1.0
