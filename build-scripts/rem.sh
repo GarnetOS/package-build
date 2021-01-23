7b9fd28d99d810a9a748ddd660cc92e209bf5e8e
radimkohout <radimm.kohout@gmail.com>
2021-01-23 14:26:14 +0100

    Create rem.sh

---
+++ build-scripts/rem.sh
@@ -0,0 +1,18 @@
+# DIR1 is the main directory
+# DIR2 is from where files will get deleted.
+DIR1=chroot-old
+DIR2="chroot"
+
+# Take a file from $DIR1 and check for it in $DIR2
+for i in $DIR1/*; do
+    HASH=$(md5sum $i 2>/dev/null | cut -d ' ' -f1 )
+    if [ "$HASH" ]; then
+        for j in $DIR2/*; do
+            HASH2=$(md5sum $j | cut -d ' ' -f1)
+            if [ "$HASH" = "$HASH2" ]; then
+                # Delete files from $DIR2
+                rm $j
+            fi
+        done
+    fi
+done
