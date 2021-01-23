a2ef93f41046d510c2ab2ec3fecc9ef962f029be
radimkohout <radimm.kohout@gmail.com>
2021-01-23 20:32:23 +0100

    Update docbook-3.1-dtd.sh

--- build-scripts/docbook-3.1-dtd.sh
+++ build-scripts/docbook-3.1-dtd.sh
@@ -1,12 +1,12 @@
 build-scripts/prepare.sh
-wget http://www.docbook.org/sgml/3.1/docbk31.zip
-tar -xvf docbk31.zip
-cd docbk31
 install_deps sgml_common
 install_deps unzip
 if [ -z "$old" ];
 then
        exit
+wget http://www.docbook.org/sgml/3.1/docbk31.zip
+tar -xvf docbk31.zip
+cd docbk31
 sed -i -e '/ISO 8879/d' \
        -e 's|DTDDECL "-//OASIS//DTD DocBook V3.1//EN"|SGMLDECL|g' \
        docbook.cat
