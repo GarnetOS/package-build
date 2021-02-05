990a6ecf46ddecef631d762d619662b3ab51b93e
radimkohout <radimm.kohout@gmail.com>
2021-02-05 20:49:43 +0100

    Update join.sh

--- build-scripts/join.sh
+++ build-scripts/join.sh
@@ -8,6 +8,6 @@ find $DIR2 -type f | xargs md5sum 2>/dev/null | sort > /tmp/m2
 
 join /tmp/m1 /tmp/m2 > /tmp/m3
 cat /tmp/m3 | cut -d ' ' -f3 | sudo xargs rm -f
-find chroot -xtype l -delete
+sudo find chroot -xtype l -delete
 # To delete empty directories
 #find $DIR2 -type d -exec sudo rm -rf {} +
