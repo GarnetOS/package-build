6510a31ab421838a9404b19e20b1342715bf512a
radimkohout <radimm.kohout@gmail.com>
2021-02-06 12:03:15 +0100

    Update join.sh

--- build-scripts/join.sh
+++ build-scripts/join.sh
@@ -1,4 +1,4 @@
-# DIR1 is the main directory
+# DIR1 is the backup directory
 # DIR2 is from where files will get deleted. Change the values accordingly
 DIR1="chroot-old"
 DIR2="chroot"
@@ -9,5 +9,4 @@ find $DIR2 -type f | xargs md5sum 2>/dev/null | sort > /tmp/m2
 join /tmp/m1 /tmp/m2 > /tmp/m3
 cat /tmp/m3 | cut -d ' ' -f3 | sudo xargs rm -f
 sudo find chroot -xtype l -delete
-# To delete empty directories
-#find $DIR2 -type d -exec sudo rm -rf {} +
+sudo find chroot -type d -empty -delete
